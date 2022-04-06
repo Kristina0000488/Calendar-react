a76257da8f453307b81ddea688d6e7fd3e552ae8
grdvsng <grdvsng@gmail.com>
2022-04-06 14:07:14 +0400

    fix heroku

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const STATIC_DIRS = [
     STATIC_ROOT, 
     path.join( STATIC_ROOT, 'static' ) 
 ];
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 const app    = express( )
 const router = express.Router( );
